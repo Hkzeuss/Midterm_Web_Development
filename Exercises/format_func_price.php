2090c9deb467b89f5a89247437621173f79d2de1
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2023-10-17 16:57:13 +0700

    Update

--- Exercises/format_func_price.php
+++ Exercises/format_func_price.php
@@ -2,31 +2,27 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Incorporate user defined function into HTML code</title>
+  <title>Incorporate user-defined function into HTML code</title>
 </head>
 <body>
-  If you purchase 2 items for $24, the amount of money is 
-  <?php
-  $amount1 = price(24, 2);
-  echo "\${$amount1}";
-  ?>
-  <br>
-  If you purchase 5 items for $12, the amount of money is 
-  <?php
-  $amount2 = price(12, 5);
-  echo "\${$amount2}";
-  ?>
-
   <?php
   function price($unitprice, $number) {
     $shipping = 5;
     $totalprice = $unitprice * $number;
+    
     // Shipping fee is $5 if the total price is less than $50
     if ($totalprice < 50) {
       $totalprice += $shipping;
     }
+    
     return $totalprice;
   }
+
+  $amount1 = price(24, 2);
+  $amount2 = price(12, 5);
   ?>
+  
+  <p>If you purchase 2 items for $24, the total amount is $<?php echo $amount1; ?></p>
+  <p>If you purchase 5 items for $12, the total amount is $<?php echo $amount2; ?></p>
 </body>
 </html>
